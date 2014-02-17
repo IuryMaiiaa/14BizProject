24def30ea002fcf1670375cfee2c5d6d9c6cb2f7
Neils01 <iurryy@yahoo.com.br>
2014-02-17 11:51:58 -0300

    adicionanto e executando tela inicial, bug setScreenSize

--- biztuto/MainActivity.java
+++ biztuto/MainActivity.java
@@ -1,15 +1,39 @@
 package com.example.biztuto;
 
+import org.cocos2d.layers.CCScene;
+import org.cocos2d.nodes.CCDirector;
+import org.cocos2d.opengl.CCGLSurfaceView;
+
+import Telas.TitleScreen;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		
+		setRequestedOrientation(
+				ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(
+				WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		
+		CCGLSurfaceView glSurfaceview = new CCGLSurfaceView(this);
+		setContentView(glSurfaceview);
+		CCDirector.sharedDirector().attachInView(glSurfaceview);
+	
+		CCDirector.sharedDirector().setScreenSize(320, 480);
+		
+		CCScene scene = new TitleScreen().scene();
+		CCDirector.sharedDirector().runWithScene(scene);
+	
 	}
 
 	@Override
